709da2bf6ddcba1db3fbe63bc56aacea784071fb
miqbaltrim <muhammadiqbaltrimulyono@gmail.com>
2023-07-15 15:13:27 +0700

    commit iqbal

--- resources/views/edit.blade.php
+++ resources/views/edit.blade.php
@@ -4,13 +4,14 @@
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Laravel CRUD With Multiple Image Upload</title>
+        <title>Baca RIED</title>
 
       <!-- Bootstrap CSS -->
      <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
      <!-- Font-awesome -->
      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
-
+     <link rel="icon" type="image/svg+xml" href="/images/Ried-favicon.svg">
+     
     </head>
     <body>
 
